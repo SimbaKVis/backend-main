441b33cd9b9d108d0120eb1ed3fbb639370e7d9f
Jono <simbakadirire@gmail.com>
2025-06-06 11:55:09 +0200

    updated db.js sql connecter

--- config/db.js
+++ config/db.js
@@ -3,8 +3,15 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
+
